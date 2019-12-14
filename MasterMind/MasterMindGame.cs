f7bff79a83fd98f813165f1d35887863047ad541
unknown <dominicbartel@gmail.com>
2019-12-13 21:49:09 -0500

    first commit

---
+++ MasterMind/MasterMindGame.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace MasterMind
+{
+    internal class MasterMindGame
+    {
+        internal int DigitRange { get; private set; }
+        internal int TriesAllowed { get; private set; }
+        internal bool IsRunning { get; private set; }
+
+        internal string CorrectNumbers { get; private set; }
+
+        internal string[] AllAttempts { get; private set; }
+
+        public int TriesAttempted { get; private set; }
+        public bool GoodEntry { get; internal set; }
+
+        public MasterMindGame(int digitRange, int triesAllowed)
+        {
+            this.DigitRange = digitRange;
+            this.TriesAllowed = triesAllowed;
+            this.TriesAttempted = triesAllowed;
+            this.IsRunning = true;
+            this.AllAttempts = new string[this.TriesAllowed];
+            this.GoodEntry = true;
+            SetCorrectNumbers();
+        }
+
+        private void SetCorrectNumbers()
+        {
+            Random getNumber = new Random();
+            this.CorrectNumbers = getNumber.Next(0, 999999).ToString("D6");
+
+        }
+
+        internal void SolveAttempt(string fourDigits)
+        {
+            if (GoodAttempt())
+            {
+                this.AllAttempts[this.TriesAttempted - 1] = fourDigits;
+                this.TriesAttempted--;
+                this.GoodEntry = true;
+            }
+            else
+            {
+                this.GoodEntry = false;
+            }
+            
+            
+        }
+
+        internal string LastAttemptResults()
+        {
+            string lastAttempt = "";
+            int correctLocationPresent = 0;
+            int incorrectLocationPresent = 0;
+            string currentAttempt = AllAttempts[TriesAttempted];
+
+            if (this.GoodEntry)
+            {
+                for (int i = 0; i < currentAttempt.Length; i++)
+                {
+                    if (currentAttempt[i] == CorrectNumbers[i])
+                    {
+                        correctLocationPresent++;
+                    }
+                    else
+                    {
+                        string removedNumberAttempt = currentAttempt.Replace(currentAttempt[i], '\0');
+                        string currentCheckedNumber = CorrectNumbers[i].ToString();
+                        if (removedNumberAttempt.Contains(currentCheckedNumber))
+                        {
+                            incorrectLocationPresent++;
+                        }
+                    }
+                }
+
+                for (int i = 0; i < correctLocationPresent; i++)
+                {
+                    lastAttempt += "+";
+                }
+
+                for (int i = 0; i < incorrectLocationPresent; i++)
+                {
+                    lastAttempt += "-";
+                }
+            }
+            
+            return lastAttempt;
+
+        }
+
+        private bool GoodAttempt()
+        {
+            bool attempt = true;
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+            return attempt;
+        }
+    }
+}
---
+++ MasterMind/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterMind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MasterMindGame masterMind = new MasterMindGame(6, 10);
+            Action<string> cw = Console.WriteLine;
+
+            while (masterMind.IsRunning)
+            {
+                Console.Clear();
+                if(masterMind.TriesAttempted == masterMind.TriesAllowed)
+                {
+                    cw("Welcome to MasterMind. You have" + masterMind.TriesAllowed+ "Attempts.");
+                }
+                else
+                {
+                    if (masterMind.GoodEntry)
+                    {
+
+                    }
+                    else
+                    {
+                        cw("Selection Results: " + masterMind.LastAttemptResults());
+                    }
+                    
+                }
+
+                cw("Please enter four digits ranging 1-6");
+
+                
+                masterMind.SolveAttempt(Console.ReadLine());
+            }
+            
+        }
+    }
+}
